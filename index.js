883655a63671333184f727e292e22f2bb6cd76f8
Dagashiya1995 <s14t212@stmail.eng.kagawa-u.ac.jp>
2017-04-19 17:12:17 +0900

    Implement slanting judgment

--- index.js
+++ index.js
@@ -13,23 +13,43 @@
   const gameIsEnd = () => {
     const array = generateMultipleArray();
     let judgment = false;
+    let victoryColor;
     //detectThree
     for ( let i = 0; i < array.length; i++ ) {
       for ( let j = 0; j < array.length; j++ ) {
+
+        //Insert color if color exit
         if (array[i][j]) {
-          let victoryColor = array[i][j];
+          victoryColor = array[i][j];
+        }
+        else {
+          continue;
         }
         //lengthJudgment
         if ( i === 0 ) {
-          if ( array[i+1][j] == victoryColor && array[i+2][j] == victoryColor ) {
+          if ( array[i+1][j] === victoryColor && array[i+2][j] === victoryColor ) {
             judgment = true;
           }
         }
 
         //widthJudgment
         if ( j === 0 ) {
-          if ( array[i][j+1] == victoryColor && array[i][j+2] == victoryColor ) {
+          if ( array[i][j+1] === victoryColor && array[i][j+2] === victoryColor ) {
             judgment = true;
+          }
+        }
+        
+        //slantingJudgment
+        if ( j === 0 ) {
+          if ( i === 0 ) {
+            if ( array[i+1][j+1] === victoryColor && array[i+2][j+2] === victoryColor ) {
+              judgment = true;
+            }
+          }
+          if ( i === 2 ) {
+            if ( array[i-1][j+1] === victoryColor && array[i-2][j+2] === victoryColor ) {
+              judgment = true;
+            }
           }
         }
       }
